ea44afce429deaf39b04f02c599970f16fbbebed
MarcusKarty2 <m.karty236796@simivalleyusd.org>
2023-01-27 14:23:58 -0800

    Content

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import easygui as eg
+
+
 health = 100    # Health of Character, [Default Value: 100]
 age = 60    # Age of Character
-
+money = 1000   # Money of Character, [Default Value: 100]
+#################################################################################################################################################################################
+    
+    # Dialog Boxes
+    
 print("You have entered the Elderly Stage of your life.")   # The last stage of your life.
 
 print("Tasks will now become harder and more physically demanding. After completing these tasks, you will have a chance to have a Heart Attack.")
@@ -8,34 +15,80 @@ print("Having a heart attack increases your chance of dying early. It may take y
 
 print("You are now " + str(age) + " years old.")    # Displays the current age of your character.
 print("You have " + str(health) + " health.")   # Displays the current health of your character.
+#################################################################################################################################################################################
 
-# Health Costs
-# If you don't purchase health insurance near the beggining stages of life you will have to pay a large fee depending on the accident.
- 
+# **MEDICAL AND HOSPITAL**
+
+# If you don't purchase health insurance near the beggining stages of life you will have to pay a fee depending on the accident.
+
+ # Health Costs
 heartAttack = 300    # Hospital fee.
 carCrash = 1000  # Car crashes can lead to buying a new car, or paying for damage.
-# If health < 25, you will die immediately. Otherwise you will have to pay a large fee depending on the damage to your body.
-
+# If health < 25 bofore the accident, you will die immediately. Otherwise you will have to pay a fee depending on the damage to your body.
 physicalTherapy = 100  # Physical therapy can lead to buying a new car
 
 # Depending on what car you purchase, the more health it has. If you crash an expensive car you have to pay more to repair it.
-# Elderly have a higher chance to get into car crashes based on your awareness or age.
+# Elderly have a higher chance to get into car crashes based on your awareness and reaction time.
 
 # If you don't decide to continue driving, then you can hire people with your money. If you do not have a car or money you can decide to take a loan from the bank.
 
-# Upper body health.
+# If the health of your head or neck are less than 25 hp then you will suffer diminishing returns.
 head = 100
 neck = 100
 
+# If the health of your arms are less than 25 hp then you will suffer diminishing returns.
 leftArm = 100
 rightArm = 100
-chest = 100
 
+# If the health of your chest, stomach, or hip is less than 25 hp then you will suffer diminishing returns.
+chest = 100
 stomache = 100
 hip = 100
 
+# If the health of your legs are less than 25 hp then you will suffer diminishing returns.
 leftLeg = 100
 rightLeg = 100
 
+# If the health of your feet are than 25 hp then you will suffer diminishing returns.
 leftFoot = 100
-rightFoot = 100
+rightFoot = 100 
+#################################################################################################################################################################################
+def groceryStore():
+    
+    global money    # Imports the global variable money to the grocery store function.
+
+    # Purchasable items from the grocery store.
+    
+    # Produce
+    lettuce =
+    onion = 
+    tomato = 
+    apple = 
+    orange = 
+    lemon = 
+    pear = 
+    grapes = 
+    peppers = 
+    pickles = 
+    carrot =
+    potatoe =
+    raddish = 
+    squash = 
+    strawberry = 
+    blueberry = 
+    blackberry = 
+    squash = 
+    
+    # Canned foods
+     
+     
+    # Meats
+    chicken =
+    beef = 
+    turkey = 
+    lamb = 
+    pork = 
+    
+    # Seafood
+    
+    
