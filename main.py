99d8c9adf8dfabe62a802d9c902917cac3469365
Marcus Karty <m.karty236796@simivalleyusd.org>
2023-01-30 12:53:23 -0800

    Physical therapy no longer results is buying a new car. | Added doctor's office.

--- main.py
+++ main.py
@@ -9,29 +9,41 @@ money = 1000  # Money of Character, [Default Value: 100]
 
 print("You have entered the Elderly Stage of your life.")  # The last stage of your life.
 
-print(
-    "Tasks will now become harder and more physically demanding. After completing these tasks, you will have a chance to have a Heart Attack.")
-print(
-    "Having a heart attack increases your chance of dying early. It may take years off you life, take away your max health, lower your health, or cause you to die instantly.")
+print("""Tasks will now become harder and more physically demanding.
+    After completing these tasks, you will have a chance to have a Heart Attack.""")
 
-print("You are now " + str(age) + " years old.")  # Displays the current age of your character.
+print("""Having a heart attack increases your chance of dying early.
+    It may take years off you life, take away your max health, lower your health, or cause you to die instantly.""")
+
+print("You are " + str(age) + " years old.")  # Displays the current age of your character.
+
 print("You have " + str(health) + " health.")  # Displays the current health of your character.
-#################################################################################################################################################################################
+
+print("You have $" + str(money))
+
+########################################################################################################################
 
 # **MEDICAL AND HOSPITAL**
 
 # If you don't purchase health insurance near the beginning stages of life you will have to pay a fee depending on the accident.
 
 # Health Costs
-heartAttack = 300  # Hospital fee.
-carCrash = 1000  # Car crashes can lead to buying a new car, or paying for damage.
-# If health < 25 before the accident, you will die immediately. Otherwise, you will have to pay a fee depending on the damage to your body.
-physicalTherapy = 100  # Physical therapy can lead to buying a new car
+hospitalVisit = 500  # Hospital fee.
+
+physicalTherapy = 200   # Physical Therapy fee.
+# Physical therapy can lead to a minor to major health increase after an accident.
+
+doctorsOffice = 150  # A doctor's checkup can lead to the discovery of a(n) unknown disease or injury.
+
+carAccident = 100  # Car crashes can lead to buying a new car, or paying for damage.
+# If health < 25 before the accident, you will die immediately.
+# Otherwise, you will have to pay a fee depending on the damage to your body.
 
 # Depending on what car you purchase, the more health it has. If you crash an expensive car you have to pay more to repair it.
 # Elderly have a higher chance to get into car crashes based on your awareness and reaction time.
 
-# If you don't decide to continue driving, then you can hire people with your money. If you do not have a car or money you can decide to take a loan from the bank.
+# If you don't decide to continue driving, then you can hire people with your money.
+# If you do not have a car or money you can decide to take a loan from the bank.
 
 # If the health of your head or neck are less than 25 hp then you will suffer diminishing returns.
 head = 100
@@ -55,8 +67,8 @@ leftFoot = 100
 rightFoot = 100
 
 
-#################################################################################################################################################################################
-def groceryStore():
+########################################################################################################################
+def groceryStore():  # Saving for future use. | Can be used in other stages of life.
     global money  # Imports the global variable money to the grocery store function.
 
     # Purchasable items from the grocery store.
