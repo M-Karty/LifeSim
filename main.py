1dc0351171f7347c2e4bec91d3f8e2b5eb9c883b
Marcus Karty <m.karty236796@simivalleyusd.org>
2023-01-31 10:08:49 -0800

    New titleLimbs(), Spelling fixes & grammar fixes.

--- main.py
+++ main.py
@@ -1,52 +1,65 @@
-# Future versions of LifeSim will add more items and reatures!
+# Future versions of LifeSim will add more items and features!
 
-# TODO: Menus, Gui, Inventory, Shops, Grammer Checks & Typos, More Stores, Item, Food Recipes, Body Health System, etc.
+# ToADD: Menus, Gui, Inventory, Shops, Grammar Checks & Typos, More Stores, Item, Food Recipes, Body Health System, etc.
 
 import easygui as eg
 
 health = 100  # Health of Character, [Default Value: 100]
 age = 60  # Age of Character
-money = 1000  # Money of Character, [Default Value: 100]
-#------------------------------------------------------------------------------------------#
+money = 1000  # Money of Character, [Default Value: 1000]
+
+
+# ------------------------------------------------------------------------------------------#
 
 # Title Screen
 
-def titleIntro():
+def titleIntro():  # Welcome page explaining the current stage of life.
     global money, age, health
-    title = "LifeSim | Welcome to the Elderly Stage of Life" # Title of Dialog Box, [Default Value: "LifeSim"]
+    title = "LifeSim | Welcome to the Elderly Stage of Life"  # Title of Dialog Box, [Default Value: "LifeSim"]
     # Displays Stats of Character + Dialog
     stats = "\t\tYou have entered the Elderly Stage of your life.\n\t" \
-        +"Tasks will now become harder and more physically demanding.\n" \
-            +"|------------------------------------------------------------------------------|" \
-                +"\n\n\t\t\t\tMoney: $" + str(money) \
-                    +"\n\t\t\t\tHealth: " + str(health)+' ' \
-                        + "hp"+"\n\t\t\t\tAge: "+ str(age) + " years"
-    eg.msgbox(stats, title,)
+            + "Tasks will now become harder and more physically demanding.\n" \
+            + "|------------------------------------------------------------------------------|" \
+            + "\n\n\t\t\t\tMoney: $" + str(money) \
+            + "\n\t\t\t\tHealth: " + str(health) + ' ' \
+            + "hp" + "\n\t\t\t\tAge: " + str(age) + " years"
+    eg.msgbox(stats, title, )
+
+
 titleIntro()
 
-def titleHealth():
-    
-    title = "LifeSim | New Health System & Challenges " # Title of Dialog Box, [Default Value: "LifeSim"]
-    
-    stats = "\t\t\t\tNew Health System\n\n" \
-        +" Heart Attacks - Having a heart attack increases your chance of dying early." \
-            +" It may take years off your life, take away your max health, lower your health, or cause you to die instantly.\n\n" \
-                +" Dementia - causes you to forget your tasks easier.\n\n" \
-                    +" Prone to illness - You catch illnesses faster & diseases last longer.\n\n" \
-                        +" Weak Joints - You are now unable to perform physically demanding activities.\n\n" \
-                            +" Decreased Awareness - Prone to more accidents & clumsiness."                  
-    eg.msgbox(stats, title,)
+
+def titleHealth():  # Page explaining new challenges
+    title = "LifeSim | New Challenges "  # Title of Dialog Box, [Default Value: "LifeSim"]
+
+    stats = "\t\t\t\tNew Challenges\n\n" \
+            + " Heart Attacks - Having a heart attack increases your chance of dying early." \
+            + " It may take years off your life, take away your max health, lower your health, or cause you to die instantly.\n\n" \
+            + " Dementia - causes you to forget your tasks easier.\n\n" \
+            + " Prone to illness - You catch illnesses faster & diseases last longer.\n\n" \
+            + " Weak Joints - You are now unable to perform physically demanding activities.\n\n" \
+            + " Decreased Awareness - Prone to more accidents & clumsiness."
+    eg.msgbox(stats, title)
+
 
 titleHealth()
-#------------------------------------------------------------------------------------------#
 
-print("""Tasks will now become harder and more physically demanding.
-After completing these tasks, you will have a chance to have a Heart Attack.""")
 
-print("""Having a heart attack increases your chance of dying early.
-It may take years off you life, take away your max health, lower your health, or cause you to die instantly.""")
+def titleLimbs():  # Page explaining the health system
+    title = "LifeSim | New Health System"
 
-#------------------------------------------------------------------------------------------#
+    stats = "\t\t\t\tNew Health System\n\n" \
+            + " A new health system has been implemented into the game!" \
+            + " All limbs now have their own health, and" \
+            + " it is now possible to break or hurt limbs." \
+            + " When you hurt a limb you can't perform certain tasks." \
+            + " \n\nFor example, if you break a bone you can go to the hospital to get it fixed."
+    eg.msgbox(stats, title)
+
+
+titleLimbs()
+
+# ------------------------------------------------------------------------------------------#
 
 # **MEDICAL AND HOSPITAL**
 
@@ -55,7 +68,7 @@ It may take years off you life, take away your max health, lower your health, or
 # Health Costs
 hospitalVisit = 500  # Hospital fee.
 
-physicalTherapy = 200   # Physical Therapy fee.
+physicalTherapy = 200  # Physical Therapy fee.
 # Physical therapy can lead to a minor to major health increase after an accident.
 
 doctorsOffice = 150  # A doctor's checkup can lead to the discovery of a(n) unknown disease or injury.
@@ -92,25 +105,29 @@ leftFoot = 100
 rightFoot = 100
 
 
-#------------------------------------------------------------------------------------------#
+# ------------------------------------------------------------------------------------------#
 
 def groceryStore():  # Saving for future use. | Can be used in other stages of life.
     global money  # Imports the global variable money to the grocery store function.
 
+    # ____ = 0 ?   0 = placeholder price
+
     # Purchasable items from the grocery store.
 
+    # ---------------------------------------------#
+
     # Drinks & Liquids Isle {
-    
+
     # Liquids {
-    water = 0 
+    water = 0
     lemonJuice = 0
     limeJuice = 0
-    
-    #---------------------------------------------#
-    
+
+    # ---------------------------------------------#
+
     # Produce Isle
-    
-    #Vegetables {
+
+    # Vegetables {
     lettuce = 0
     onion = 0
     tomato = 0
@@ -130,16 +147,16 @@ def groceryStore():  # Saving for future use. | Can be used in other stages of l
     blackberry = 0
     squash = 0
 
-    #---------------------------------------------#
-    
+    # ---------------------------------------------#
+
     # Bakery Isle
-    
+
     # Breads {
     bread = 0
     bagel = 0
     baguette = 0
     donut = 0
-    
+
     # Grain {
     wheat = 0
     flour = 0
@@ -147,10 +164,10 @@ def groceryStore():  # Saving for future use. | Can be used in other stages of l
     rice = 0
     barley = 0
 
-    #---------------------------------------------#
-    
+    # ---------------------------------------------#
+
     # Meats Isle
-    
+
     # Meat {
     chicken = 0
     beef = 0
@@ -158,17 +175,17 @@ def groceryStore():  # Saving for future use. | Can be used in other stages of l
     lamb = 0
     pork = 0
 
-    #---------------------------------------------#
-    
+    # ---------------------------------------------#
+
     # Seafood Isle
-    
+
     # Crustations {
     shrimp = 0
     crab = 0
     clam = 0
     muscle = 0
     lobster = 0
-    
+
     # Fish & Misc {
     tilapia = 0
     squid = 0
@@ -178,12 +195,12 @@ def groceryStore():  # Saving for future use. | Can be used in other stages of l
     trout = 0
     grouper = 0
 
-    #---------------------------------------------#
-    
+    # ---------------------------------------------#
+
     # Spice Isle
-    
+
     # Spices {
-    salt = 0 
+    salt = 0
     pepper = 0
     basil = 0
     cumin = 0
@@ -197,36 +214,35 @@ def groceryStore():  # Saving for future use. | Can be used in other stages of l
     ginger = 0
     mustardSeed = 0
     vanilla = 0
-    
-    #---------------------------------------------#
-    
+
+    # ---------------------------------------------#
+
     # Dairy Isle
-    
+
     # Dairy {
     milk = 0
     eggs = 0
     butter = 0
     creamer = 0
     dehydratedMilk = 0
-    
+
     # Cheese {
     parmesan = 0
     threeCheeseBlend = 0
     cheddar = 0
     mozzerella = 0
-    
-    #---------------------------------------------#
-        
+
+    # ---------------------------------------------#
+
     # Pasta Isle
-    
+
     # Pasta {
     angelHair = 0
     speghetti = 0
     elbow = 0
     bowtie = 0
-    
+
     # Sauce {
     redSauce = 0
-    
-    #---------------------------------------------#
- 
+
+    # ---------------------------------------------#
